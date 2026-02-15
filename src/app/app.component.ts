347ad170ca9659f84901861ec9ae019af755cc46
monukumar <kumarmonusonu987@gmail.com>
2026-02-15 17:41:38 +0530

    changed mapKeycloakProfile to custom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,4 @@
 
-// import { Component, OnInit } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-// import { FooterComponent } from './shared/components/footer/footer.component';
-// import { NotificationComponent } from './shared/components/notification/notification.component';
-// import { CommonModule } from '@angular/common';
-// import { SharedModule } from './shared/shared.module';
-// import { HeaderComponent } from './shared/components/header/header.component';
-// import { KeycloakService } from 'keycloak-angular';
-// import { Store } from '@ngrx/store';
-// import * as AuthActions from './store/actions/auth.actions';
-// import { Observable } from 'rxjs';
-// import { selectIsAuthenticated } from './store/selectors/auth.selectors';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [
-//     CommonModule,
-//     RouterOutlet,
-//     HeaderComponent,
-//     SidebarComponent,
-//     FooterComponent,
-//     NotificationComponent,
-//     SharedModule,
-//   ],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.scss',
-// })
-// export class AppComponent implements OnInit {
-//   title = 'banking-app';
-
-//    // ✅ Observable for authentication status
-//   isAuthenticated$: Observable<boolean>;
-
-//   constructor(
-//     private keycloakService: KeycloakService,
-//     private store: Store
-//   ) {
-//     this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
-//   }
-
-//   async ngOnInit() {
-//     // ✅ Sync Keycloak authentication state with NgRx
-//     try {
-//       const isLoggedIn = await this.keycloakService.isLoggedIn();
-
-//       if (isLoggedIn) {
-//         const userProfile = await this.keycloakService.loadUserProfile();
-//         this.store.dispatch(AuthActions.loginSuccess({ user: userProfile }));
-//         this.store.dispatch(
-//           AuthActions.setAuthenticated({ authenticated: true })
-//         );
-//       } else {
-//         this.store.dispatch(
-//           AuthActions.setAuthenticated({ authenticated: false })
-//         );
-//       }
-//     } catch (error) {
-//       console.error('Failed to sync auth state on app init', error);
-//     }
-//   }
-// }
-
 import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -76,6 +12,7 @@ import { SidebarComponent } from './shared/components/sidebar/sidebar.component'
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NotificationComponent } from './shared/components/notification/notification.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { mapKeycloakProfileToUserProfile } from './core/models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -112,8 +49,10 @@ export class AppComponent implements OnInit {
         const isLoggedIn = await this.keycloakService.isLoggedIn();
 
         if (isLoggedIn) {
-          const userProfile = await this.keycloakService.loadUserProfile();
-          this.store.dispatch(AuthActions.loginSuccess({ user: userProfile }));
+          const keycProfile = await this.keycloakService.loadUserProfile();
+          const mappedKeycloakProfile = mapKeycloakProfileToUserProfile(keycProfile);
+
+          this.store.dispatch(AuthActions.loginSuccess({ user: mappedKeycloakProfile }));
           this.store.dispatch(AuthActions.setAuthenticated({ authenticated: true }));
         } else {
           this.store.dispatch(AuthActions.setAuthenticated({ authenticated: false }));
